94052d34ce75cdf67f4447796c6dfb70ebe68b34
224jfarrell <224jfarrell@frhsd.com>
2023-03-07 11:54:31 -0500

    comite

---
+++ script.js
@@ -0,0 +1,9 @@
+// var link = window.location.href = 'home.html';
+
+// function redirect(){
+//     setInterval(link, 30000)
+// }
+
+function intervala(){
+    document.getElementById('q').disabled = true;
+}

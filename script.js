c7b61923625b033a8b0654ee12f02a00a07852d1
224jfarrell <224jfarrell@frhsd.com>
2023-03-24 10:40:57 -0400

    animation update

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function redirect(){
     window.location.href = 'home.html'
 }
 
+function modal() {
+    $(document).ready(function(){
+        $("#exampleModal").modal('show');
+    });
+};
+setTimeout(modal, 2000);
+var newText = Math.floor(Math.random()*1000000000000000);
+$('#discount-code').html(newText)
